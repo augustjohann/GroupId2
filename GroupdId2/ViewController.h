94c4a7ce33e084feca85f9a6fe36272a49b4c557
Markus Schmid <schmidgpms@t-online.de>
2015-01-14 17:18:28 +0100

    Initial Commit

---
+++ GroupdId2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GroupdId2
+//
+//  Created by Markus Schmid on 14.01.15.
+//  Copyright (c) 2015 Markus Schmid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GroupdId2/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GroupdId2
+//
+//  Created by Markus Schmid on 14.01.15.
+//  Copyright (c) 2015 Markus Schmid. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
